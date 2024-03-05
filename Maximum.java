28c7783c7294f1b4c5170b74446a31f32fadd0ec
Yocifer <151704081+Yocifer@users.noreply.github.com>
2024-03-05 10:28:35 +0800

    Add files via upload

---
+++ Maximum.java
@@ -0,0 +1,68 @@
+public class Maximum 
+{
+    public static int maxima(int [] array)
+    {
+        int m = 0;
+        int i=1;
+        while ( i < array.length)
+        {
+            if (array[i] > array[m])
+            {
+                m = i;
+            } 
+            i++;
+        }
+        return m;
+    }
+    public static int maximum(int [] array)
+    {
+        int temmaximum = array[0];
+        int i=1;
+        while ( i < array.length)
+        {
+            if (array[i] > temmaximum)
+               temmaximum = array[i];
+            i++;
+        }
+         
+        return temmaximum;
+    }
+    
+    public static int minima(int[] array)
+    {
+        int minIndex = 0;
+        int i = 1;
+        while (i < array.length)
+        {
+            if (array[i] < array[minIndex])
+            {
+                minIndex = i;
+            } 
+            i++;
+        }
+        return minIndex;
+    }
+    
+    public static int minimum(int[] array)
+    {
+        int min = array[0];
+        int i = 1;
+        while (i < array.length)
+        {
+            if (array[i] < min)
+               min = array[i];
+            i++;
+        }
+        return min;
+    }
+    
+    // test driver
+    public static void main(String[] args) 
+    {
+       int [] arr = new int[]{342,34,233,456};
+       System.out.println(maximum(arr));
+       System.out.println(maxima(arr));
+       System.out.println(minimum(arr));
+       System.out.println(minima(arr)); 
+    }
+}
